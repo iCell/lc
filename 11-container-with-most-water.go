c30be324404ea5368864b4db84bbed100902ce94
Shawn Lee <shawn@Shawns-MacBook-Pro-2.local>
2020-05-22 15:31:44 +0900

    solve 11 second time

--- 11-container-with-most-water.go
+++ 11-container-with-most-water.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 func maxArea1(height []int) int {
 	max := 0
 	for i := 0; i < len(height)-1; i++ {
@@ -41,4 +43,33 @@ func maxArea2(height []int) int {
 	}
 
 	return max
+}
+
+func min(a, b int) int {
+	return int(math.Min(float64(a), float64(b)))
+}
+
+func max(a, b int) int {
+	return int(math.Max(float64(a), float64(b)))
+}
+
+func maxAreaAgain(height []int) int {
+	var (
+		i = 0
+		j = len(height) - 1
+	)
+	maxArea := 0
+	for {
+		if i >= j {
+			break
+		}
+
+		maxArea = max(maxArea, (j-i)*min(height[i], height[j]))
+		if height[i] > height[j] {
+			j--
+		} else {
+			i++
+		}
+	}
+	return maxArea
 }
