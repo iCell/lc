36d956bbb0f68a9cc35f71cdf3d3edfd02cebfa1
iCell <icell.vip@gmail.com>
2020-05-24 22:10:48 +0900

    solved plus-one problem

--- 66-plus-one.go
+++ 66-plus-one.go
@@ -1,5 +1,65 @@
 package main
 
-func plusOne(digits []int) []int {
+import "fmt"
 
+func main() {
+	fmt.Println(plusOne([]int{9, 9, 9}))
+}
+
+type Stack struct {
+	values []int
+}
+
+func NewStack() *Stack {
+	return &Stack{}
+}
+
+func (s *Stack) isEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *Stack) push(v int) {
+	s.values = append(s.values, v)
+}
+
+func (s *Stack) pop() int {
+	if len(s.values) == 0 {
+		return -1
+	}
+	x, v := s.values[len(s.values)-1], s.values[:len(s.values)-1]
+	s.values = v
+	return x
+}
+
+func plusOne(digits []int) []int {
+	st := NewStack()
+
+	plus := true
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		digit := digits[i]
+		if plus {
+			digit++
+		}
+		if digit == 10 {
+			plus = true
+			st.push(0)
+			if i == 0 {
+				st.push(1)
+			}
+		} else {
+			plus = false
+			st.push(digit)
+		}
+	}
+
+	var result []int
+	for {
+		p := st.pop()
+
+		if p == -1 {
+			return result
+		}
+		result = append(result, p)
+	}
 }
---
+++ 88-merge-sorted-array.go
@@ -0,0 +1,9 @@
+package main
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	index1, index2 := 0, 0
+
+	for i := 0; i < n; i++ {
+
+	}
+}
