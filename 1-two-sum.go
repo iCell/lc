2522397d3854714a47a24778dc63e151c71818de
iCell <icell.vip@gmail.com>
2020-05-26 11:21:48 +0900

    redo two-sum second time

--- 1-two-sum.go
+++ 1-two-sum.go
@@ -1,41 +1,21 @@
 package main
 
-// stupid solution
 func twoSum1(nums []int, target int) []int {
 	for i := 0; i < len(nums)-1; i++ {
 		for j := 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				if i != j {
-					return []int{i, j}
-				}
+			if i != j && nums[i]+nums[j] == target {
+				return []int{i, j}
 			}
 		}
 	}
 	return nil
 }
 
-// less stupid
 func twoSum2(nums []int, target int) []int {
 	temp := make(map[int]int, len(nums))
 	for i, num := range nums {
-		temp[num] = i
-	}
-
-	for i, num := range nums {
-		another := target - num
-		j, ok := temp[another]
-		if ok && i != j {
-			return []int{i, j}
-		}
-	}
-	return nil
-}
-
-func twoSum3(nums []int, target int) []int {
-	temp := make(map[int]int, len(nums))
-	for i, num := range nums {
-		j, ok := temp[target-num]
-		if ok {
+		j, exist := temp[target-num]
+		if exist {
 			return []int{i, j}
 		}
 		temp[num] = i
