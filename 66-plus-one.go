330ee3e75c627cd9934d05712e81fe50ecb9f04f
Shawn Lee <shawn@Shawns-MacBook-Pro-2.local>
2020-05-26 15:46:31 +0900

    redo plus one second time

--- 66-plus-one.go
+++ 66-plus-one.go
@@ -3,63 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(plusOne([]int{9, 9, 9}))
-}
-
-type Stack struct {
-	values []int
-}
-
-func NewStack() *Stack {
-	return &Stack{}
-}
-
-func (s *Stack) isEmpty() bool {
-	return len(s.values) == 0
-}
-
-func (s *Stack) push(v int) {
-	s.values = append(s.values, v)
-}
-
-func (s *Stack) pop() int {
-	if len(s.values) == 0 {
-		return -1
-	}
-	x, v := s.values[len(s.values)-1], s.values[:len(s.values)-1]
-	s.values = v
-	return x
+	fmt.Println(plusOne([]int{1, 2, 3}))
 }
 
 func plusOne(digits []int) []int {
-	st := NewStack()
-
 	plus := true
-
+	length := len(digits) + 1
+	result := make([]int, length, length)
 	for i := len(digits) - 1; i >= 0; i-- {
-		digit := digits[i]
+		v := digits[i]
 		if plus {
-			digit++
-		}
-		if digit == 10 {
-			plus = true
-			st.push(0)
-			if i == 0 {
-				st.push(1)
+			if v == 9 {
+				result[i+1] = 0
+				plus = true
+			} else {
+				result[i+1] = v + 1
+				plus = false
 			}
 		} else {
-			plus = false
-			st.push(digit)
+			result[i+1] = v
 		}
 	}
-
-	var result []int
-	for {
-		p := st.pop()
-
-		if p == -1 {
-			return result
-		}
-		result = append(result, p)
+	if plus {
+		result[0] = 1
+		return result
 	}
+	return result[1:]
 }
