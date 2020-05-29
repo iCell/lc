960c7a9fb41e1b829fa6f24d599e3f9bf801a0a3
Shawn Lee <shawn@Shawns-MacBook-Pro-2.local>
2020-05-29 17:24:50 +0900

    redo n-ary preorder and postorder

--- 289-n-ary-preorder-traversal.go
+++ 289-n-ary-preorder-traversal.go
@@ -20,3 +20,49 @@ func preorder(root *Node) []int {
 	}
 	return result
 }
+
+func preorder(root *Node) []int {
+	stack := NewStack()
+	var result []int
+	node := root
+
+	stack.push(node)
+
+	for !stack.isEmpty() {
+		pop := stack.pop()
+		if pop != nil {
+			result = append(result, pop.Val)
+			for i := len(pop.Children) - 1; i >= 0; i-- {
+				node := pop.Children[i]
+				stack.push(node)
+			}
+		}
+	}
+
+	return result
+}
+
+type Stack struct {
+	values []*Node
+}
+
+func NewStack() *Stack {
+	return &Stack{}
+}
+
+func (s *Stack) isEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *Stack) push(v *Node) {
+	s.values = append(s.values, v)
+}
+
+func (s *Stack) pop() *Node {
+	if len(s.values) == 0 {
+		return nil
+	}
+	x, v := s.values[len(s.values)-1], s.values[:len(s.values)-1]
+	s.values = v
+	return x
+}
--- 590-n-ary-postorder-traversal.go
+++ 590-n-ary-postorder-traversal.go
@@ -16,3 +16,53 @@ func postorder(root *Node) []int {
 	result = append(result, root.Val)
 	return result
 }
+
+func postorder2(root *Node) []int {
+	stack := NewStack()
+	var result []int
+	node := root
+
+	stack.push(node)
+
+	for !stack.isEmpty() {
+		pop := stack.pop()
+		if pop != nil {
+			result = append(result, pop.Val)
+			for i := 0; i < len(pop.Children); i++ {
+				node := pop.Children[i]
+				stack.push(node)
+			}
+		}
+	}
+
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
+
+	return result
+}
+
+type Stack struct {
+	values []*Node
+}
+
+func NewStack() *Stack {
+	return &Stack{}
+}
+
+func (s *Stack) isEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *Stack) push(v *Node) {
+	s.values = append(s.values, v)
+}
+
+func (s *Stack) pop() *Node {
+	if len(s.values) == 0 {
+		return nil
+	}
+	x, v := s.values[len(s.values)-1], s.values[:len(s.values)-1]
+	s.values = v
+	return x
+}
