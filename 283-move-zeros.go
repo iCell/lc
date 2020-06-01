76fa927cc5e9948d2dc36ebd2a35af14ceece185
Shawn Lee <shawn@Shawns-MacBook-Pro-2.local>
2020-06-01 17:47:23 +0900

    redo move zeros third time

--- 283-move-zeros.go
+++ 283-move-zeros.go
@@ -4,12 +4,8 @@ func moveZeroes(nums []int) {
 	index := 0
 	for i, num := range nums {
 		if num != 0 {
-			// swap num and nums[index]
-			var temp = nums[index]
-			nums[index] = num
-			nums[i] = temp
-
-			index += 1
+			nums[index], nums[i] = nums[i], nums[index]
+			index++
 		}
 	}
 }
