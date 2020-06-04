1a6b9242a7aa4609415ec4dbecb2571d830b28b0
Shawn Lee <shawn@MacBook-Pro.local>
2020-06-04 13:21:48 +0900

    use multiple ways to solve invert binary tree

--- 226-invert-binary-tree.go
+++ 226-invert-binary-tree.go
@@ -18,3 +18,57 @@ func invertTree(root *TreeNode) *TreeNode {
 
 	return root
 }
+
+func invertTree2(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	invertTree2(root.Left)
+	root.Left, root.Right = root.Right, root.Left
+	invertTree2(root.Left)
+
+	return root
+}
+
+func invertTree3(root *TreeNode) *TreeNode {
+	node := root
+	stack := NewStack()
+	for len(stack.values) != 0 || node != nil {
+		for node != nil {
+			stack.push(node)
+			node = node.Left
+		}
+		top := stack.pop()
+
+		top.Left, top.Right = top.Right, top.Left
+		node = top.Left
+	}
+
+	return root
+}
+
+type Stack struct {
+	values []*TreeNode
+}
+
+func NewStack() *Stack {
+	return &Stack{}
+}
+
+func (s *Stack) isEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *Stack) push(v *TreeNode) {
+	s.values = append(s.values, v)
+}
+
+func (s *Stack) pop() *TreeNode {
+	if len(s.values) == 0 {
+		return nil
+	}
+	x, v := s.values[len(s.values)-1], s.values[:len(s.values)-1]
+	s.values = v
+	return x
+}
