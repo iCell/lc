e6e8b393ecbd587bccacc8f8a6f51401794ffe77
Shawn Lee <shawn@MacBook-Pro.local>
2020-06-06 16:34:55 +0900

    use backtrack to solve combination problem

--- 77-combinations.go
+++ 77-combinations.go
@@ -22,3 +22,22 @@ func combine(n int, k int) [][]int {
 	result = append(result, combine(n-1, k)...)
 	return result
 }
+
+func combine2(n int, k int) [][]int {
+	var result [][]int
+	helper(&result, n, k, 1, []int{})
+	return result
+}
+
+func helper(res *[][]int, n int, k int, begin int, temp []int) {
+	if len(temp) == k {
+		*res = append(*res, append([]int{}, temp...))
+		return
+	}
+
+	for i := begin; i <= n; i++ {
+		temp = append(temp, i)
+		helper(res, n+1, k, i+1, temp)
+		temp = temp[:len(temp)-1]
+	}
+}
