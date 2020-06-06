131db04510b2d66a7f5a9f48b08e41e355c55a38
Shawn Lee <shawn@MacBook-Pro.local>
2020-06-06 16:20:28 +0900

    use backtrack to solve generate parentheses

--- 22-generate-parentheses.go
+++ 22-generate-parentheses.go
@@ -22,3 +22,27 @@ func generate(res *[]string, left int, right int, str string) {
 		generate(res, left, right-1, str+")")
 	}
 }
+
+func generateParenthesis2(n int) []string {
+	result := make([]string, 0)
+	helper(&result, n, n, "")
+	return result
+}
+
+func helper(res *[]string, left, right int, result string) {
+	if left == 0 && right == 0 {
+		*res = append(*res, result)
+		return
+	}
+
+	if left > 0 {
+		result = result + "("
+		helper(res, left-1, right, result)
+		result = result[:len(result)-1]
+	}
+	if left < right {
+		result = result + ")"
+		helper(res, left, right-1, result)
+		result = result[:len(result)-1]
+	}
+}
