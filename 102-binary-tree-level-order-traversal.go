726ee091b830b0ced44b7af066f8a24e66c13dff
Shawn Lee <shawn@MacBook-Pro.local>
2020-06-08 16:07:33 +0900

    solve level tarversal problems

---
+++ 102-binary-tree-level-order-traversal.go
@@ -0,0 +1,65 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	var result [][]int
+	queue := &Queue{}
+
+	queue.InQueue(root)
+	for !queue.IsEmpty() {
+		nodes := make([]*TreeNode, 0, queue.Size())
+		for {
+			node := queue.DeQueue()
+			if node == nil {
+				break
+			}
+			nodes = append(nodes, node)
+		}
+
+		values := make([]int, 0, queue.Size())
+		for _, node := range nodes {
+			values = append(values, node.Val)
+			if node.Left != nil {
+				queue.InQueue(node.Left)
+			}
+			if node.Right != nil {
+				queue.InQueue(node.Right)
+			}
+		}
+		if len(values) != 0 {
+			result = append(result, values)
+		}
+	}
+
+	return result
+}
+
+type Queue struct {
+	values []*TreeNode
+}
+
+func (q *Queue) InQueue(n *TreeNode) {
+	q.values = append(q.values, n)
+}
+
+func (q *Queue) DeQueue() *TreeNode {
+	if q.IsEmpty() {
+		return nil
+	}
+	first, values := q.values[0], q.values[1:]
+	q.values = values
+	return first
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.values) == 0
+}
+
+func (q *Queue) Size() int {
+	return len(q.values)
+}
---
+++ 515-find-largest-value-in-each-tree-row.go
@@ -0,0 +1,71 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func largestValues(root *TreeNode) []int {
+	var result []int
+	queue := &Queue{}
+
+	queue.InQueue(root)
+	for !queue.IsEmpty() {
+
+		nodes := make([]*TreeNode, 0, queue.Size())
+		for {
+			node := queue.DeQueue()
+			if node == nil {
+				break
+			}
+			nodes = append(nodes, node)
+
+		}
+
+		if len(nodes) == 0 {
+			continue
+		}
+
+		max := nodes[0].Val
+		for i, node := range nodes {
+			if i > 0 && node.Val > max {
+				max = node.Val
+			}
+			if node.Left != nil {
+				queue.InQueue(node.Left)
+			}
+			if node.Right != nil {
+				queue.InQueue(node.Right)
+			}
+		}
+		result = append(result, max)
+	}
+
+	return result
+}
+
+type Queue struct {
+	values []*TreeNode
+}
+
+func (q *Queue) InQueue(n *TreeNode) {
+	q.values = append(q.values, n)
+}
+
+func (q *Queue) DeQueue() *TreeNode {
+	if q.IsEmpty() {
+		return nil
+	}
+	first, values := q.values[0], q.values[1:]
+	q.values = values
+	return first
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.values) == 0
+}
+
+func (q *Queue) Size() int {
+	return len(q.values)
+}
