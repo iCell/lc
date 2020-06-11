22eae58e0499d10ec15acecbbc555d5e7990a2bf
Shawn Lee <shawn@MacBook-Pro.local>
2020-06-11 17:13:31 +0900

    use bfs to solve number of islands

--- 200-number-of-islands.go
+++ 200-number-of-islands.go
@@ -29,3 +29,72 @@ func dfs(row int, column int, grid [][]byte) {
 	dfs(row-1, column, grid)
 	dfs(row, column-1, grid)
 }
+
+func numIslands2(grid [][]byte) int {
+	if len(grid) == 0 {
+		return 0
+	}
+
+	numr, numc := len(grid), len(grid[0])
+	nums := 0
+	queue := Queue{}
+
+	for r, rows := range grid {
+		for c, v := range rows {
+			if v == byte('0') {
+				continue
+			}
+			nums++
+			grid[r][c] = byte('0')
+			queue.InQueue([]int{r, c})
+			for !queue.IsEmpty() {
+				size := queue.Size()
+				for size > 0 {
+					p := queue.DeQueue()
+					x, y := p[0], p[1]
+					if x+1 < numr && grid[x+1][y] == byte('1') {
+						grid[x+1][y] = byte('0')
+						queue.InQueue([]int{x + 1, y})
+					}
+					if y+1 < numc && grid[x][y+1] == byte('1') {
+						grid[x][y+1] = byte('0')
+						queue.InQueue([]int{x, y + 1})
+					}
+					if x-1 >= 0 && grid[x-1][y] == byte('1') {
+						grid[x-1][y] = byte('0')
+						queue.InQueue([]int{x - 1, y})
+					}
+					if y-1 >= 0 && grid[x][y-1] == byte('1') {
+						grid[x][y-1] = byte('0')
+						queue.InQueue([]int{x, y - 1})
+					}
+					size--
+				}
+			}
+		}
+	}
+
+	return nums
+}
+
+type Queue struct {
+	values [][]int
+}
+
+func (q *Queue) InQueue(n []int) {
+	q.values = append(q.values, n)
+}
+
+func (q *Queue) DeQueue() []int {
+	first, values := q.values[0], q.values[1:]
+	q.values = values
+	return first
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.values) == 0
+}
+
+func (q *Queue) Size() int {
+	return len(q.values)
+}
