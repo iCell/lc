9ca44112cadf700fcb51fa503f33140e00c3e388
Shawn Lee <shawn@MacBook-Pro.local>
2020-06-17 14:12:30 +0900

    need test hashmap

--- 127-word-latter.go
+++ 127-word-latter.go
@@ -49,6 +49,91 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 	return 0
 }
 
+func ladderLength2(beginWord string, endWord string, wordList []string) int {
+	if beginWord == endWord {
+		return 0
+	}
+
+	words := NewHashSet()
+	for _, w := range wordList {
+		words.Add(w)
+	}
+
+	length := 1
+
+	beginSet, endSet, visited := NewHashSet(), NewHashSet(), NewHashSet()
+	beginSet.Add(beginWord)
+	endSet.Add(endWord)
+
+	for !beginSet.IsEmpty() && !endSet.IsEmpty() {
+		if beginSet.Size() > endSet.Size() {
+			beginSet, endSet = endSet, beginSet
+		}
+		temp := NewHashSet()
+		for _, v := range beginSet.Values() {
+			runes := []rune(v)
+			for i, old := range runes {
+				for c := 'a'; c <= 'z'; c++ {
+					if c == old {
+						continue
+					}
+					runes[i] = c
+
+					next := string(runes)
+					if endSet.Contain(next) {
+						return length + 1
+					}
+					if visited.Contain(next) || !words.Contain(next) {
+						continue
+					}
+					temp.Add(next)
+					visited.Add(next)
+					runes[i] = old
+				}
+			}
+		}
+		beginSet = temp
+		length++
+	}
+
+	return 0
+}
+
+type HashSet struct {
+	values map[string]bool
+}
+
+func NewHashSet() *HashSet {
+	return &HashSet{
+		values: make(map[string]bool),
+	}
+}
+
+func (set *HashSet) Add(s string) {
+	set.values[s] = true
+}
+
+func (set *HashSet) Contain(s string) bool {
+	_, ok := set.values[s]
+	return ok
+}
+
+func (set *HashSet) IsEmpty() bool {
+	return len(set.values) == 0
+}
+
+func (set *HashSet) Size() int {
+	return len(set.values)
+}
+
+func (set *HashSet) Values() []string {
+	var r []string
+	for k, _ := range set.values {
+		r = append(r, k)
+	}
+	return r
+}
+
 type Queue struct {
 	values []string
 }
