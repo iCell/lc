7a478e8901f4e052729cf0dab65e1b9541cafce5
iCell <icell.vip@gmail.com>
2020-06-21 17:34:30 +0900

    redo add two numbers using more efficient way

--- 2-add-two-numbers.go
+++ 2-add-two-numbers.go
@@ -52,3 +52,44 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 	return root.Next
 }
+
+func addTwoNumbers2(l1 *ListNode, l2 *ListNode) *ListNode {
+	root := &ListNode{}
+	current := root
+	carry := 0
+
+	node1, node2 := l1, l2
+	for node1 != nil || node2 != nil {
+		v1, v2 := 0, 0
+		if node1 != nil {
+			v1 = node1.Val
+		}
+		if node2 != nil {
+			v2 = node2.Val
+		}
+
+		val := v1 + v2 + carry
+		if val >= 10 {
+			val = val % 10
+			carry = 1
+		} else {
+			carry = 0
+		}
+		node := &ListNode{Val: val}
+
+		current.Next = node
+		current = current.Next
+
+		if node1 != nil {
+			node1 = node1.Next
+		}
+		if node2 != nil {
+			node2 = node2.Next
+		}
+	}
+
+	if carry == 1 {
+		current.Next = &ListNode{Val: 1}
+	}
+	return root.Next
+}
