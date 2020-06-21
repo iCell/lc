78a8cb2ad72ab7b7300f252afad696823921e87e
Shawn Lee <shawn@MacBook-Pro.local>
2020-06-21 15:32:31 +0900

    redo search 2d matrix second time

--- 74-search-2d-matrix.go
+++ 74-search-2d-matrix.go
@@ -4,38 +4,28 @@ func searchMatrix(matrix [][]int, target int) bool {
 	if len(matrix) == 0 || len(matrix[0]) == 0 {
 		return false
 	}
-	index := -1
-	left, right := 0, len(matrix)-1
-	for left <= right {
-		mid := left + (right-left)>>1
+	lr, rr := 0, len(matrix)-1
+	for lr <= rr {
+		mr := lr + (rr-lr)>>1
 
-		mr := matrix[mid]
-
-		if target >= mr[0] && target <= mr[len(mr)-1] {
-			index = mid
-		}
-		if target > mr[len(mr)-1] {
-			left = mid + 1
+		if target > matrix[mr][len(matrix[mr])-1] {
+			lr = mr + 1
+		} else if target < matrix[mr][0] {
+			rr = mr - 1
 		} else {
-			right = mid - 1
-		}
-	}
-
-	if index == -1 {
-		return false
-	}
-
-	row := matrix[index]
-	left, right = 0, len(row)-1
-	for left <= right {
-		mid := (left + right) / 2
-		if target == row[mid] {
-			return true
-		}
-		if target > row[mid] {
-			left = mid + 1
-		} else {
-			right = mid - 1
+			row := matrix[mr]
+			lr, rr = 0, len(row)-1
+			for lr <= rr {
+				mr := lr + (rr-lr)>>1
+				if row[mr] == target {
+					return true
+				} else if row[mr] > target {
+					rr = mr - 1
+				} else {
+					lr = mr + 1
+				}
+			}
+			return false
 		}
 	}
 
