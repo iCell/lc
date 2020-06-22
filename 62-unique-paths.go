106d29aa5c0b8036d1757271f6d88600f63737e7
Shawn Lee <shawn@MacBook-Pro.local>
2020-06-22 17:04:45 +0900

    solved unique paths

---
+++ 62-unique-paths.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println(uniquePaths(3, 2))
+}
+
+func uniquePaths(m int, n int) int {
+    cache := make([][]int, m)
+    for i := 0; i < m; i++ {
+        cache[i] = make([]int, n)
+        cache[i][0] = 1
+    }
+    for i := 0; i < n; i++ {
+        cache[0][i] = 1
+    }
+    for i := 1; i < m; i++ {
+        for j := 1; j < n; j++ {
+            cache[i][j] = cache[i-1][j] + cache[i][j-1]
+        }
+    }
+    return cache[m-1][n-1]
+}
