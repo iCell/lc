412c4357d9fc3fbdf02c5012360b51441f6720fe
Shawn Lee <shawn@MacBook-Pro.local>
2020-06-22 16:28:41 +0900

    redo genetic mutation

--- 433-minimum-genetic-mutation.go
+++ 433-minimum-genetic-mutation.go
@@ -72,3 +72,67 @@ func (q *Queue) IsEmpty() bool {
 func (q *Queue) Size() int {
 	return len(q.values)
 }
+
+func minMutation2(start string, end string, bank []string) int {
+	if start == end {
+		return 0
+	}
+
+	genetics := make(map[string]bool)
+	for _, b := range bank {
+		genetics[b] = true
+	}
+
+	_, valid := genetics[end]
+	if !valid {
+		return -1
+	}
+
+	charset := []rune{'A', 'C', 'G', 'T'}
+
+	level := 0
+	starts, ends := make(map[string]bool), make(map[string]bool)
+	starts[start] = true
+	ends[end] = true
+
+	for len(starts) > 0 && len(ends) > 0 {
+		if len(starts) > len(ends) {
+			starts, ends = ends, starts
+		}
+
+		temp := make(map[string]bool)
+
+		for gen := range starts {
+			runes := []rune(gen)
+			for i, old := range runes {
+				for _, c := range charset {
+					if c == old {
+						continue
+					}
+					runes[i] = c
+
+					next := string(runes)
+
+					_, exist := ends[next]
+					if exist {
+						return level + 1
+					}
+
+					_, ok := genetics[next]
+					if !ok {
+						continue
+					}
+
+					delete(genetics, next)
+					temp[next] = true
+				}
+				runes[i] = old
+			}
+		}
+
+		starts = temp
+		level++
+	}
+
+	return -1
+}
