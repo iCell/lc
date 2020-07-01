e9d0f9d9e815fe5ca96608437bf6375c4333b2d4
Shawn Lee <shawn@Shawns-MacBook-Pro.local>
2020-07-01 22:53:06 +0900

    redo longest common subsequence

--- 1143-longest-common-subsequence.go
+++ 1143-longest-common-subsequence.go
@@ -15,3 +15,24 @@ func helper(r1, r2 []rune, li, ri int) int {
 	}
 	return int(math.Max(float64(helper(r1, r2, li, ri-1)), float64(helper(r1, r2, li-1, ri))))
 }
+
+func longestCommonSubsequence2(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1, m+1)
+	for i := range dp {
+		arr := make([]int, n+1, n+1)
+		dp[i] = arr
+	}
+
+	for i := 1; i < m+1; i++ {
+		for j := 1; j < n+1; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = int(math.Max(float64(dp[i-1][j]), float64(dp[i][j-1])))
+			}
+		}
+	}
+
+	return dp[m][n]
+}
