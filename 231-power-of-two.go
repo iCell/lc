30104f21d191774ac86ed1f30c554f2f562106b4
Shawn Lee <shawn@MacBook-Pro.local>
2020-07-08 10:20:50 +0900

    solved power of two

---
+++ 231-power-of-two.go
@@ -0,0 +1,15 @@
+func isPowerOfTwo2(n int) bool {
+	if n <= 0 {
+		return false
+	}
+
+	for n%2 == 0 {
+		n = n / 2
+	}
+
+	return n == 1
+}
+
+func isPowerOfTwo(n int) bool {
+	return n > 0 && (n&(n-1)) == 0
+}
