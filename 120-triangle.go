68fd14f12c496c7cf86d0c96cf0b12bda6e75471
Shawn Lee <shawn@lixiaoyus-MacBook-Pro.local>
2020-07-16 16:03:42 +0900

    solved triangle using another approach

--- 120-triangle.go
+++ 120-triangle.go
@@ -3,7 +3,6 @@
 // f[i][0] = f[i-1][0] + current
 // f[i][i] = f[i-1][j-1] + current
 // f[i][j] = min(f[i-1][j-1], f[i-1][j]) + current
-
 func minimumTotal(triangle [][]int) int {
     if len(triangle) == 0 {
         return 0
@@ -33,6 +32,31 @@ func minimumTotal(triangle [][]int) int {
         result = min(result, dp[len(triangle)-1][i])
     }
     return result
+}
+
+// f[n][i] = min(f[n+1][i], f[n+1][i+1]) + current
+// f[max][i] = f[max][i]
+func minimumTotal2(triangle [][]int) int {
+    if len(triangle) == 0 {
+        return 0
+    }
+
+    dp := make([][]int, len(triangle), len(triangle))
+    for i := range dp {
+        dp[i] = make([]int, len(triangle[i]), len(triangle[i]))
+    }
+
+    for i := range dp[len(dp)-1] {
+        dp[len(dp)-1][i] = triangle[len(dp)-1][i]
+    }
+
+    for i := len(dp) - 2; i >= 0; i-- {
+        for j := 0; j <= len(dp[i])-1; j++ {
+            dp[i][j] = min(dp[i+1][j], dp[i+1][j+1]) + triangle[i][j]
+        }
+    }
+
+    return dp[0][0]
 }
 
 func max(x, y int) int {
