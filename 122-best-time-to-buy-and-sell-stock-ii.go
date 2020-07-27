8b161e636a5a0e9e8f5a7dc1a8286fcadbe17ec1
Shawn Lee <shawn@Shawns-MacBook-Pro.local>
2020-07-27 19:42:56 +0900

    solved stock problem using dynamic programming

--- 122-best-time-to-buy-and-sell-stock-ii.go
+++ 122-best-time-to-buy-and-sell-stock-ii.go
@@ -10,3 +10,25 @@ func maxProfit(prices []int) int {
 	}
 	return max
 }
+
+func maxProfit2(prices []int) int {
+	dp := make([][]int, len(prices), len(prices))
+	for i := range dp {
+		dp[i] = make([]int, 2, 2)
+	}
+	dp[0][0], dp[0][1] = 0, -prices[0]
+
+	for i := 1; i < len(dp); i++ {
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+	}
+
+	return dp[len(prices)-1][0]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
