98c5d0cefb6094fa436be42f88fc8ae718786b90
Shawn Lee <shawn@lixiaoyus-MacBook-Pro.local>
2020-07-30 14:10:45 +0900

    solved maximum subarray using dynamic programming

--- 53-maximum-subarray.go
+++ 53-maximum-subarray.go
@@ -13,3 +13,28 @@ func maxSubArray(nums []int) int {
     }
     return max
 }
+
+func maxSubArray(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+
+    dp := make([]int, len(nums), len(nums))
+    dp[0] = nums[0]
+
+    sum := dp[0]
+    for i := 1; i < len(dp); i++ {
+        dp[i] = max(dp[i-1]+nums[i], nums[i])
+        sum = max(sum, dp[i])
+    }
+
+    return sum
+}
+
+func max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
+
