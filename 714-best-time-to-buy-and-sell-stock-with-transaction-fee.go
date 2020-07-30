b044b7ad9fcb7bd5ee05ab7cf8dbaee604654c9e
Shawn Lee <shawn@lixiaoyus-MacBook-Pro.local>
2020-07-30 14:53:11 +0900

    solved stock problem with transaction fee

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee.go
@@ -0,0 +1,31 @@
+// dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i] - fee)
+// dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+
+func maxProfit(prices []int, fee int) int {
+    if len(prices) == 0 {
+        return 0
+    }
+
+    dp := make([][]int, len(prices), len(prices))
+    for i := range dp {
+        dp[i] = make([]int, 2, 2)
+    }
+    dp[0][0], dp[0][1] = 0, -prices[0]
+
+    var profit int
+    for i := 1; i < len(dp); i++ {
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i]-fee)
+        dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+
+        profit = max(profit, dp[i][0])
+    }
+
+    return profit
+}
+
+func max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
