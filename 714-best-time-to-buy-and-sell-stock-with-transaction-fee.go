38b29fcd1053fe2f54bb591b474bd82614165073
Shawn Lee <shawn@Shawns-MacBook-Pro.local>
2020-08-02 23:19:10 +0900

    use another dynamic programming functions

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee.go
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee.go
@@ -29,3 +29,22 @@ func max(x, y int) int {
     }
     return y
 }
+
+// dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i] - fee)
+// dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+func maxProfit2(prices []int, fee int) int {
+    dp := make([][]int, len(prices), len(prices))
+    for i := range dp {
+        dp[i] = make([]int, 2, 2)
+    }
+    dp[0][0], dp[0][1] = 0, -prices[0]-fee
+
+    var result int
+    for i := 1; i < len(dp); i++ {
+        dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i]-fee)
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+        result = max(result, dp[i][0])
+    }
+
+    return result
+}
