07e5f24ee2df80164ddb16bcdc6f9b75bf299e35
Shawn Lee <shawn@lixiaoyus-MacBook-Pro.local>
2020-08-05 16:42:38 +0900

    use another way to solve stock problem

--- 123-best-time-to-buy-and-sell-stock-iii.go
+++ 123-best-time-to-buy-and-sell-stock-iii.go
@@ -1,3 +1,5 @@
+import "math"
+
 // 第 i 天，还剩下 k 次可以买，手里有股票：dp[i][k][1]
 // i-1 天，手里就有股票 dp[i-1][k][1]
 // i-1 天，手里没有股票，第 i 天买了股票 dp[i-1][k-1][0] - price[i]
@@ -66,6 +68,83 @@ func maxProfit(prices []int) int {
     var profit int
     for k := 2; k > 0; k-- {
         profit = max(profit, dp[len(dp)-1][k])
+    }
+
+    return profit
+}
+
+// k 代表还剩下几次购买机会，那么总共能够进行买卖的次数为 k*2
+// dp[i][k]
+// if k != 0 && k % 2 == 1: max(dp[i-1][k], dp[i-1][k-1] - prices[i])
+// if k != 0 && k % 2 == 0: max(dp[i-1][k], dp[i-1][k-1] + prices[i])
+//
+// dp[0][0] = 0, dp[0][1] = -prices[0]
+
+func maxProfit(prices []int) int {
+    if len(prices) == 0 {
+        return 0
+    }
+
+    times := 2
+
+    dp := make([][]int, len(prices), len(prices))
+    for i := range dp {
+        dp[i] = make([]int, times*2+1, times*2+1)
+    }
+    for k := 1; k <= times*2; k++ {
+        if k%2 == 1 {
+            dp[0][k] = -prices[0]
+        }
+    }
+
+    for i := 1; i < len(dp); i++ {
+        for k := 0; k <= times*2; k++ {
+            if k%2 == 0 {
+                if k == 0 {
+                    dp[i][0] = dp[i-1][0]
+                } else {
+                    dp[i][k] = max(dp[i-1][k], dp[i-1][k-1]+prices[i])
+                }
+            } else {
+                dp[i][k] = max(dp[i-1][k], dp[i-1][k-1]-prices[i])
+            }
+        }
+    }
+
+    var profit int
+    for i := range dp[len(dp)-1] {
+        if i%2 == 0 {
+            profit = max(profit, dp[len(dp)-1][i])
+        }
+    }
+
+    return profit
+}
+
+func maxProfit(prices []int) int {
+    if len(prices) == 0 {
+        return 0
+    }
+
+    times := 2*2 + 1
+
+    dp := make([]int, times, times)
+    for i := range dp {
+        dp[i] = math.MinInt64
+    }
+    dp[0], dp[1] = 0, -prices[0]
+
+    var profit int
+    for i := 1; i < len(prices); i++ {
+        for k := 1; k < times; k++ {
+            switch {
+            case k%2 == 1:
+                dp[k] = max(dp[k], dp[k-1]-prices[i])
+            case k%2 == 0:
+                dp[k] = max(dp[k], dp[k-1]+prices[i])
+                profit = max(dp[k], profit)
+            }
+        }
     }
 
     return profit
