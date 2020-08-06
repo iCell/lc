a4d9721473efafdb0d7e7b16de3d9540bea04f90
Shawn Lee <shawn@Shawns-MacBook-Pro.local>
2020-08-06 22:08:30 +0900

    solved symmetric tree

---
+++ 101-symmetric-tree.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	return helper(root, root)
+}
+
+func helper(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	return left.Val == right.Val && helper(left.Right, right.Left) && helper(left.Left, right.Right)
+}
