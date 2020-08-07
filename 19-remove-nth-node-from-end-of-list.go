31f12d61b0bfaa668d8237480daffdea2f3d9f60
Shawn Lee <shawn@lixiaoyus-MacBook-Pro.local>
2020-08-07 16:59:00 +0900

    use another way to solve remove nth node

--- 19-remove-nth-node-from-end-of-list.go
+++ 19-remove-nth-node-from-end-of-list.go
@@ -20,3 +20,22 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 
     return head
 }
+
+func removeNthFromEnd2(head *ListNode, n int) *ListNode {
+    slow, fast := head, head
+    for i := 0; i < n; i++ {
+        fast = fast.Next
+    }
+    if fast == nil {
+        return slow.Next
+    }
+
+    for fast.Next != nil {
+        fast = fast.Next
+        slow = slow.Next
+    }
+
+    slow.Next = slow.Next.Next
+
+    return head
+}
