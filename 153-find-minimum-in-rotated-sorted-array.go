6aa8bb8361947ae65530c6385bf8a77ff7dbc897
Shawn Lee <shawn@Shawns-MacBook-Pro.local>
2020-08-26 16:50:39 +0900

    solved find minimum in rotated sorted array

---
+++ 153-find-minimum-in-rotated-sorted-array.go
@@ -0,0 +1,17 @@
+func findMin(nums []int) int {
+    if len(nums) == 1 || nums[len(nums)-1] > nums[0] {
+        return nums[0]
+    }
+
+    left, right := 0, len(nums)-1
+    for left < right {
+        mid := (left + right) / 2
+        if nums[mid] > nums[right] {
+            left = mid + 1
+        } else if nums[mid] < nums[right] {
+            right = mid
+        }
+    }
+
+    return nums[right]
+}
