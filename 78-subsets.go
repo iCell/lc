5fce656ea18757c5462ee6bd604bba9f1430dbfc
Shawn Lee <shawn@Shawns-MacBook-Pro.local>
2020-09-04 11:18:11 +0900

    use another way to solve subsets

--- 78-subsets.go
+++ 78-subsets.go
@@ -17,3 +17,17 @@ func helper(result *[][]int, level, max int, nums, temp []int) {
 	helper(result, level+1, max, nums, temp)
 	temp = temp[:len(temp)-1]
 }
+
+func helper2(nums []int, index int, current []int, result *[][]int) {
+	*result = append(*result, append([]int{}, current...))
+
+	for i := index; i < len(nums); i++ {
+		// [1] -> [1, 2]
+		current = append(current, nums[i])
+		// [1, 2] -> [1, 2, 3]
+		// [1, 2] -> [1, 2, 4]
+		helper(nums, i+1, current, result)
+		// [1, 2] -> [1]s
+		current = current[:len(current)-1]
+	}
+}
