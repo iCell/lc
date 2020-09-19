f7a6457b067ff05fd84a0421701272f15a1337d2
iCell <icell.vip@gmail.com>
2020-09-19 12:51:59 +0800

    solved clone graph using dfs

--- 133-clone-graph.go
+++ 133-clone-graph.go
@@ -24,3 +24,39 @@ func cloneGraph(node *Node) *Node {
 
     return visited[node]
 }
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Neighbors []*Node
+ * }
+ */
+
+func cloneGraph(node *Node) *Node {
+    visited := make(map[*Node]*Node)
+    dfs(node, visited)
+    return visited[node]
+}
+
+func dfs(node *Node, visited map[*Node]*Node) *Node {
+    if node == nil {
+        return nil
+    }
+
+    if visited[node] != nil {
+        return visited[node]
+    }
+
+    clone := &Node{Val: node.Val, Neighbors: make([]*Node, 0)}
+    visited[node] = clone
+
+    for _, neighbor := range node.Neighbors {
+        clone.Neighbors = append(
+            clone.Neighbors,
+            dfs(neighbor, visited),
+        )
+    }
+
+    return clone
+}
