103f73a82c4e914eafb3a2760527f65d89590f8e
iCell <icell.vip@gmail.com>
2020-10-02 14:10:28 +0800

    solved first bad version

---
+++ 278-first-bad-version.go
@@ -0,0 +1,20 @@
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return            true if current version is bad
+ *                    false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+    left, right := 1, n
+    for left <= right {
+        mid := (left + right) / 2
+        if isBadVersion(mid) == true {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    return left
+}
