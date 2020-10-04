302d13d2c06ce1c7e79b11f6d50785ce11caddff
iCell <icell.vip@gmail.com>
2020-10-04 18:00:51 +0800

    solved stone game using recursion

--- 877-stone-game.go
+++ 877-stone-game.go
@@ -1,3 +1,5 @@
+import "math"
+
 // 本题中，可以认为 dp[i][j] 代表从 i-j 个石头堆中，Alex 比 Lee 多的石头数，若是正的则 Alex 赢
 // 先考虑最后一步，最后一步肯定只有一个石头堆，那么谁先手谁就赢，dp[i][i] = piles[i]
 // 由于每次取石头只能从最左边或者最右边取，因此要不取 i 堆要不取 j 堆，也就是说 i-j 是好多堆石头的情况同最后只有 2 堆石头的情况是一致的，这里指考虑剩下 2 堆石头，由于最后剩下 2 堆，可以是 Alex 先手，也可以是 Lee 先手，每次取的时候都是最优解，所以 piles[0]-dp[1][1] 代表 Alex 先手取走第一批石头得分 piles[0]，而 Lee 之后取走剩下的一堆石头得分 dp[1][1]，反过来 piles[1]-dp[0][0] 代表 Lee 先手而 Alex 后手
@@ -25,4 +27,38 @@ func max(x, y int) int {
 		return x
 	}
 	return y
+}
+
+func stoneGame(piles []int) bool {
+	memo := make([][]int, len(piles), len(piles))
+	for i := range memo {
+		memo[i] = make([]int, len(piles), len(piles))
+		for j := range memo[i] {
+			memo[i][j] = math.MinInt64
+		}
+	}
+
+	return choose(piles, 0, len(piles)-1, memo) > 0
+}
+
+// 代表选择 left 到 right 堆石头，Alex 相对于 Lee 多出来的分数
+func choose(piles []int, left, right int, memo [][]int) int {
+	result := memo[left][right]
+	if result != math.MinInt64 {
+		return result
+	}
+
+	if left == right {
+		memo[left][right] = piles[left]
+		return piles[left]
+	}
+
+	// 谁先选择了 left，然后下一个人选择 left+1, right 中
+	chooseLeft := piles[left] - choose(piles, left+1, right, memo)
+	chooseRight := piles[right] - choose(piles, left, right-1, memo)
+	v := max(chooseLeft, chooseRight)
+
+	memo[left][right] = v
+
+	return v
 }
