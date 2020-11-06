fab06bbd5c04d4f07e6adf8e4f89b40e88426a76
iCell <icell.vip@gmail.com>
2020-11-06 10:15:18 +0800

    solved roman to integer

---
+++ 13-roman-to-integer.go
@@ -0,0 +1,26 @@
+var mapping = map[byte]int {
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
+}
+
+func romanToInt(s string) int {
+	var result int
+	for i := 0; i < len(s); i++ {
+		cur, next := s[i], byte(0)
+		if i + 1 < len(s) {
+			next = s[i+1]
+		}
+		if next == byte(0) || mapping[cur] >= mapping[next] {
+			result += mapping[cur]
+		} else {
+			result += mapping[next] - mapping[cur]
+			i += 1
+		}
+	}
+	return result
+}
