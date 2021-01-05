8ca4635f8d616bc6089e242f29a9d3d853b73082
iCell <icell.vip@gmail.com>
2021-01-05 16:31:48 +0800

    there is no need to use memo

--- 83-remove-duplicates-from-sorted-list.go
+++ 83-remove-duplicates-from-sorted-list.go
@@ -6,16 +6,15 @@
  * }
  */
 func deleteDuplicates(head *ListNode) *ListNode {
-	memo, dummy := make(map[int]bool), &ListNode{Next: head}
+	dummy := &ListNode{Next: head}
 	pre, current := dummy, head
 	for current != nil {
 		next := current.Next
-		if memo[current.Val] {
+		if pre != dummy && current.Val == pre.Val {
 			pre.Next = next
 		} else {
 			pre = current
 		}
-		memo[current.Val] = true
 		current = next
 	}
 	return dummy.Next
